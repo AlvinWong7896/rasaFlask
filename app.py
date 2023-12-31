0217b4b57605ef185adec85a2cd4e11cd03c558d
CK Wong <alvinwong7896@gmail.com>
2023-12-31 17:10:30 +0800

    able to access the chatbot from localhost:3000 but seem to have some issue with the Action Server i.e. bot returned msg I have entered

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
-from flask import Flask, render_template, request, jsonify, json
+# from flask import Flask, render_template, request, jsonify, json
+# import requests
+
+# RASA_API_URL = "http://localhost:5005/webhooks/rest/webhook"
+# app = Flask(__name__)
+
+
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+
+# @app.route("/webhook", methods=["POST"])
+# def webhook():
+#     user_message = request.json["message"]
+#     print("User Message:", user_message)
+
+#     # Send user message to Rasa and get chatbot's response
+#     rasa_response = requests.post(RASA_API_URL, json={"message": user_message})
+#     rasa_response_json = rasa_response.json()
+
+#     print("Rasa Response:", rasa_response_json)
+
+#     bot_response = (
+#         rasa_response_json[0]["text"]
+#         if rasa_response_json
+#         else "Sorry, I did't understand that."
+#     )
+
+#     return jsonify({"response": bot_response})
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True, port=3000)
+
+from flask import Flask, render_template, request, jsonify
 import requests
 
-RASA_API_URL = "http://localhost:5005/webhooks/rest/webhook"
+ACTION_ENDPOINT = "http://localhost:5055/webhook"
 app = Flask(__name__)
 
 
@@ -12,22 +47,34 @@ def index():
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    user_message = request.json["message"]
-    print("User Message:", user_message)
+    try:
+        user_message = request.json["message"]
+        print("User Message:", user_message)
 
-    # Send user message to Rasa and get chatbot's response
-    rasa_response = requests.post(RASA_API_URL, json={"message": user_message})
-    rasa_response_json = rasa_response_json()
+        # Send user message to the custom action server and get chatbot's response
+        action_response = requests.post(ACTION_ENDPOINT, json={"message": user_message})
+        action_response.raise_for_status()  # Raise an error for bad responses
+        action_response_json = action_response.json()
 
-    print("Rasa Response:", rasa_response_json)
+        print("Action Response:", action_response_json)
 
-    bot_response = (
-        rasa_response_json[0]["text"]
-        if rasa_response_json
-        else "Sorry, I did't  understand that."
-    )
+        if action_response_json and isinstance(action_response_json, list):
+            bot_response = action_response_json[0].get(
+                "text", "Sorry, I didn't understand that."
+            )
+        else:
+            bot_response = "Sorry, I didn't understand that."
 
-    return jsonify({"response": bot_response})
+        return jsonify({"response": bot_response})
+
+    except KeyError:
+        return jsonify({"response": "Error: 'message' key not found in the request."})
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error communicating with the custom action server: {e}")
+        return jsonify(
+            {"response": "Sorry, there was an issue processing your request."}
+        )
 
 
 if __name__ == "__main__":
